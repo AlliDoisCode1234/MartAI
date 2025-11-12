62040b0dba2c208e53b2c4a135391e07b2eaee0d
AlliDoisCode1234 <josiah.james.fowler@gmail.com>
2025-11-12 10:01:45 -0500

    Fixed favicon and manifest errors

--- martai/app/layout.tsx
+++ martai/app/layout.tsx
@@ -14,6 +14,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
